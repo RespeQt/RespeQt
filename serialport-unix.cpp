6ee1857b0e516ab994deb6fd2a75fed1c56e612e
TheMontezuma <marcin.sochacki@gmx.net>
2017-04-07 22:29:06 +0200

    simplify serial port selection

--- serialport-unix.cpp
+++ serialport-unix.cpp
@@ -61,14 +61,14 @@ StandardSerialPortBackend::~StandardSerialPortBackend()
 #ifdef Q_OS_LINUX
   QString StandardSerialPortBackend::defaultPortName()
   {
-      return QString("/dev/ttyS0");
+      return QString("ttyS0");
   }
 #endif
 
 #ifdef Q_OS_MAC
 QString StandardSerialPortBackend::defaultPortName()
 {
-    return QString("/dev/tty.usbserial");
+    return QString("tty.usbserial");
 }
 #endif
 
@@ -78,7 +78,8 @@ bool StandardSerialPortBackend::open()
         close();
     }
 
-    QString name = respeqtSettings->serialPortName();
+    QString name("/dev/");
+    name.append(respeqtSettings->serialPortName());
     mMethod = respeqtSettings->serialPortHandshakingMethod();
     mWriteDelay = SLEEP_FACTOR * respeqtSettings->serialPortWriteDelay();
     mCompErrDelay = respeqtSettings->serialPortCompErrDelay();
